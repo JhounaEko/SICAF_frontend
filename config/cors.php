fee98584f637b21dce60d86d72f61860de933bca
rollback321 <rd8995851@gmail.com>
2025-04-13 09:59:54 -0400

    Aceptar los cambios

--- config/cors.php
+++ config/cors.php
@@ -8,18 +8,13 @@ return [
     |--------------------------------------------------------------------------
     |
     | Here you may configure your settings for cross-origin resource sharing
-<<<<<<< HEAD
     | or "middleware". This determines what cross-origin operations may execute
-=======
-    | or "CORS". This determines what cross-origin operations may execute
->>>>>>> 595971771846d108dc12d3d1d63d05ae2f6206f7
     | in web browsers. You are free to adjust these settings as needed.
     |
     | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
     |
     */
 
-<<<<<<< HEAD
     'paths' => ['api/*', 'sanctum/csrf-cookie','*'],
 
     'allowed_methods' => ['GET, POST, PUT, DELETE, OPTIONS, PATCH'],
@@ -33,21 +28,6 @@ return [
     'exposed_headers' => [],
 
     'max_age' => 86400,
-=======
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
-    'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['http://dominio'],
-
-    'allowed_origins_patterns' => [],
-
-    'allowed_headers' => ['*'],
-
-    'exposed_headers' => [],
-
-    'max_age' => 0,
->>>>>>> 595971771846d108dc12d3d1d63d05ae2f6206f7
 
     'supports_credentials' => false,
 
