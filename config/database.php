1485818372534e8d1bd7f8c739b209e983b31caf
Gadiel Randall Valdez Alvarado <g.randall.v.alvarado@gmail.com>
2025-04-14 13:59:30 -0400

    Update uso esquemas +notificaciones

--- config/database.php
+++ config/database.php
@@ -98,7 +98,7 @@ return [
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
-            'search_path' => 'public',
+            'search_path' => ['accounting','items','users','public'],
             'sslmode' => 'prefer',
         ],
 
