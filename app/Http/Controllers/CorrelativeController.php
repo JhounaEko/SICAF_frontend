970751a92f9246093dd26dc3977e0c8a38e75708
Gadiel Randall Valdez Alvarado <g.randall.v.alvarado@gmail.com>
2025-04-16 11:15:01 -0400

    migraciones de la mayoria de las tablas de la nueva base de datos, exceptuando las tablas interrogantes de CONTABILIDAD

---
+++ app/Http/Controllers/CorrelativeController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Correlative;
+use Illuminate\Http\Request;
+
+class CorrelativeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Correlative $correlative)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Correlative $correlative)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Correlative $correlative)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Correlative $correlative)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/HistoricChangeController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\HistoricChange;
+use Illuminate\Http\Request;
+
+class HistoricChangeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(HistoricChange $historicChange)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(HistoricChange $historicChange)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, HistoricChange $historicChange)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(HistoricChange $historicChange)
+    {
+        //
+    }
+}
